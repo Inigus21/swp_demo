e5964768d4f7577d933f05afce5f8177c9e89c79
Inigus21 <duongnkthe186476@fpt.edu.vn>
2025-02-06 00:09:48 +0700

    feat: connect to DB

--- src/main/java/com/explorevietnam/entity/Review.java
+++ src/main/java/com/explorevietnam/entity/Review.java
@@ -3,11 +3,16 @@ package com.explorevietnam.entity;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "review")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private int tourId;
     private int accountId;
